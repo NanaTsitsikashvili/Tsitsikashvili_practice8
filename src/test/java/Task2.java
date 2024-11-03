2f8b67f5bfef12f759f9b440ec2fa255bff13a82
Nana Tsitsikashvili <ntsitsikashvili@credo.ge>
2024-11-03 20:04:20 +0400

    task3_tsitsikashvili

--- src/test/java/Task2.java
+++ src/test/java/Task2.java
@@ -1,2 +1,16 @@
-public class Task2 {
+import java.util.Scanner;
+
+class InputValidator {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("ჩაწერე რიცხვი: ");
+
+            try {
+                int number = scanner.nextInt();
+                System.out.println("შეყვანილი რიცხვი: " + number);
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Invalid input. Please enter an integer.");
+            }
+        }
+    }
 }
